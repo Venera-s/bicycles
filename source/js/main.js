151f2c23cccd4248b873a38c3646093386bd62ab
Venera <coldlives@gmail.com>
2020-11-18 00:24:37 +0300

    eslintrc

--- source/js/main.js
+++ source/js/main.js
@@ -19,12 +19,12 @@
 })();
 
 (function () {
-  var inputTel = document.getElementById('tel-field');
+  var inputTel = document.getElementById(`tel-field`);
   var MIN_TITLE_LENGTH = 22;
 
   var phoneMask = function () {
     window.iMask.phone(inputTel, {
-      mask: '+{7} (000) 000 - 00 - 00',
+      mask: "+{7} (000) 000 - 00 - 00",
     });
   };
 
@@ -34,13 +34,13 @@
     var numberLength = inputTel.value.length;
 
     if (numberLength < MIN_TITLE_LENGTH) {
-      inputTel.setCustomValidity('Введите 10 цифр номера телефона.');
+      inputTel.setCustomValidity("Введите 10 цифр номера телефона.");
     } else {
-      inputTel.setCustomValidity('');
+      inputTel.setCustomValidity("");
     }
     inputTel.reportValidity();
   };
 
-  inputTel.addEventListener('input', onInputTelInput);
+  inputTel.addEventListener("input", onInputTelInput);
 
 })();
