a01a30d6d59ca8b37500d5b7fc0a33e4df7e83cd
Venera <coldlives@gmail.com>
2020-11-18 16:38:44 +0300

    menu js

--- build/js/main.js
+++ build/js/main.js
@@ -1,22 +1,25 @@
-"use strict";
+'use strict';
 
 (function () {
-  var menuBtn = document.querySelector(".main-header__btn");
-  var menuList = document.querySelector(".main-nav");
-  var menuBack = document.querySelector(".main-header__logo-wrap");
+  var menuBtn = document.querySelector('.main-header__btn');
+  var menuList = document.querySelector('.main-nav');
+  var menuBack = document.querySelector('.main-header__logo-wrap');
+  var headerLogo = document.querySelector('.main-header__logo');
 
   var menuHide = function () {
-    menuBtn.classList.toggle("main-header__btn--open");
-    menuList.classList.toggle("main-nav--hidden");
+    menuBtn.classList.toggle('main-header__btn--open');
+    menuList.classList.toggle('main-nav--hidden');
+
   };
 
   menuHide();
 
-  menuBtn.addEventListener("click", function (evt) {
+  menuBtn.addEventListener('click', function (evt) {
     evt.preventDefault();
     menuHide();
-    menuBtn.classList.toggle("main-header__btn--close");
-    menuBack.classsList.toggle("main-header__logo-background");
+    menuBtn.classList.toggle('main-header__btn--close');
+    menuBack.classList.toggle('main-header__logo-wrap--back');
+    headerLogo.classList.toggle('main-header__logo--close');
   });
 })();
 
@@ -26,7 +29,7 @@
 
   var phoneMask = function () {
     window.iMask.phone(inputTel, {
-      mask: "+{7} (000) 000 - 00 - 00",
+      mask: '+{7} (000) 000 - 00 - 00',
     });
   };
 
@@ -36,13 +39,13 @@
     var numberLength = inputTel.value.length;
 
     if (numberLength < MIN_TITLE_LENGTH) {
-      inputTel.setCustomValidity("Введите 10 цифр номера телефона.");
+      inputTel.setCustomValidity('Введите 10 цифр номера телефона.');
     } else {
-      inputTel.setCustomValidity("");
+      inputTel.setCustomValidity('');
     }
     inputTel.reportValidity();
   };
 
-  inputTel.addEventListener("input", onInputTelInput);
+  inputTel.addEventListener('input', onInputTelInput);
 
 })();
